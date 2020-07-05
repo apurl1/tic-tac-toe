252ebfdf3b346167e4eced9fb3c38676e464543c
Rupal Nigam <rupaln@Rupals-MacBook-Pro.local>
2020-07-04 18:47:22 -0600

    Updates

---
+++ precache-manifest.6f33ab0a17fe79b9327f615536544fd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d397c23579de3afdd6391047f5c05f8",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "03d6d83868826863c6f1",
+    "url": "/tic-tac-toe/static/css/main.f6b24d66.chunk.css"
+  },
+  {
+    "revision": "5d60e11a36a4b870c7c6",
+    "url": "/tic-tac-toe/static/js/2.9fa9506b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.9fa9506b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d6d83868826863c6f1",
+    "url": "/tic-tac-toe/static/js/main.c24cc93f.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
